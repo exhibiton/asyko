455ea7fe65913c4a4973eac84a3e1d2797dc5ae7
Jesse Heikkilä <jessevheikkila@gmail.com>
2019-02-13 15:55:29 +0200

    return frontend folder to the repository

---
+++ frontend/layouts/Layout.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import Head from 'next/head'
+import '../styles/core.scss'
+
+type Props = {
+  title?: string,
+}
+
+const Layout: React.FunctionComponent<Props> = ({ children, title = 'Asyko - End-to-End Testing made easy' }) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700,800" rel="stylesheet" />
+    </Head>
+    <header>
+      <nav>
+        <div className="mlxl mts">
+          <img src={require('../assets/images/asyko logo.svg')} />
+        </div>
+      </nav>
+    </header>
+    {children}
+  </div>
+)
+
+export default Layout
